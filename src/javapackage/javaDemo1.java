52ddf9c6f2b243bc315f72e0822a5b921c0d3cf4
ROHIMASKE <144350292+ROHIMASKE@users.noreply.github.com>
2023-09-21 21:10:47 +0530

    "this is my First java Code"

---
+++ src/javapackage/javaDemo1.java
@@ -0,0 +1,16 @@
+package javapackage;
+
+public class javaDemo1 {
+	public static void main(String[] args) {
+
+		String str = "RohiniMaske";
+		int count = 0;
+
+		for (int i = 0; i <= str.length() - 1; i++) {
+			if (str.charAt(i) != ' ') {
+				count++;
+			}
+		}
+		System.out.println("Length of the given string" + count);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rohin
+ *
+ */
+module javaprogg {
+}
